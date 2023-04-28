22bcbf6d7c4808522994cbc94fae99dd0384f560
Joshua Stanford <stanjosh@gmail.com>
2023-04-28 03:19:05 -0500

    fixed note naming schema

--- notes/notes.py
+++ notes/notes.py
@@ -9,45 +9,48 @@ notes_file = os.path.join('.', "saved_notes.bin")
 notes_status = None
 
 def display_notes():
-    load_notes()
     return list(note_dict.values())
     
 def get_note_by_title(title):
     return note_dict[title]
 
 def check_for_existing(note_title):
-    if note_title:
-        try:
-            get_note_by_title(note_title)
-        except KeyError:
-            return note_title
-    return f"New Note {len(display_notes())}"
+    existing_titles = list(note for note in display_notes() if note.title.startswith(note_title))
+    title_number = len(existing_titles)
+    print(title_number)
+    return  f"{note_title} {title_number + 1}" if title_number >= 1 else note_title
+    
 
 def update_note_dict_key(note, old_title=None):
     note_dict[note.title] = note
     if old_title:
-        print (f"removed dictionary entry '{note_dict.pop(old_title)}")
-    print (note_dict)
+        delete_note(old_title)
     save_notes()
+    print (note_dict)
+
 
 class Notes:
     def __init__(self, title='', content='', **kwargs):
         self.title = title
         self.content = content
-        self.editing = kwargs.get('editing', False)
 
     def edit(self, title, content):
         old_title = self.title
         print (f'old: {old_title}')
-        self.title = check_for_existing(title)
+        self.title = title
         print (f'new: {self.title}')
         self.content = content
         print (f'new: {self.content}')
-        update_note_dict_key(note=self, old_title=old_title)
+        if not old_title == self.title:
+            self.title = check_for_existing(self.title)
+            update_note_dict_key(note=self, old_title=old_title)
+        else:
+            update_note_dict_key(note=self)
 
-    
-def add(title='New Note', content=lorem):
-    new_note = Notes(check_for_existing(title), content, editing=True)
+
+
+def add(note_title='New Note', content=lorem):
+    new_note = Notes(check_for_existing(note_title), content)
     update_note_dict_key(note=new_note)
     return new_note.title
 
@@ -73,5 +76,6 @@ def load_notes():
 def reset_notes():
     note_dict.clear()
     add('reset notes!')
+    print('reset notes')
     with open(notes_file, "wb+") as pickle_file:
         pickle.dump(note_dict, pickle_file)
