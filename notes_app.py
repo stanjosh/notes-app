fc4c71e0d0856b4e51384fe2e7cad56e09ef1c84
Debian <bitnami@ip-172-26-14-218.ec2.internal>
2023-05-13 15:39:57 +0000

    added mongodb and style

--- notes_app.py
+++ notes_app.py
@@ -1,23 +1,15 @@
 from flask import Flask, render_template, request, redirect, url_for, session, flash
-
+from pymongo import MongoClient
+from bson.objectid import ObjectId
 
 notes_app = Flask(__name__.split('.')[0])
 notes_app.secret_key = 'dev'
 lorem = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, et cetera"
 
+client = MongoClient('localhost', 27017, connect=False)
 
-def check_for_existing(note_title):
-    if note_title in session['note_dict']:
-        if note_title[-1].isdigit():
-            existing_title_number = int(note_title[-1]) + 1
-            print(existing_title_number)
-            existing_title_number = str(existing_title_number)
-            print(existing_title_number)
-        else:
-            existing_title_number = 2
-            note_title = f"{note_title.rstrip(' 0123456789')} {existing_title_number}"
-    return  note_title
-
+db = client.flask_db
+notes = db.notes
 
 
 @notes_app.route('/')
@@ -27,44 +19,61 @@ def home(error=None):
     if not 'email' in session:
         flash('please log in')
         return redirect(url_for('login'))
-    return render_template('notes.html')
+    all_notes = notes.find()
+    return render_template('notes.html', notes=all_notes)
     
 
 @notes_app.route('/add')    
 def new_note():
-    new_note = check_for_existing('New note')
-    session['note_dict'][new_note] = lorem
-    session.modified = True
-    return redirect(url_for('home'))
-
-@notes_app.route('/delete/<note_to_delete>')
-def delete_note(note_to_delete=None):
-    if note_to_delete:
-        print(f"trying to delete '{session['note_dict'].pop(note_to_delete)}")
-        flash(f"deleted {note_to_delete}")
-        session.modified = True
+    notes.insert_one({'title' : 'New Note', 'content' : lorem})
     return redirect(url_for('home'))
 
 
-@notes_app.route('/save/<note_title>')
-def note_to_save(note_title):    
-    note_title = request.args.get('title')
+@notes_app.route('/<id_to_delete>/delete')
+def delete_note(id_to_delete):
+    if id_to_delete:
+        deleted_note = notes.find_one({'_id': ObjectId(id_to_delete)})
+        deleted_note_title = deleted_note['title']
+        deleted_note = notes.delete_one({'_id': ObjectId(id_to_delete)})
+        
+        print(f"trying to delete")
+        flash(f"deleted {deleted_note_title}")
+    return redirect(url_for('home'))
+
+
+@notes_app.route('/<note_id>/save')
+def note_to_save(note_id):
     content = request.args.get('content')
-    session['note_dict'][note_title] = content
-    session.modified = True
+    note_title = request.args.get('title')    
+    notes.update_one({'_id' : ObjectId(note_id)}, 
+                        { '$set' : {
+                                'title' : note_title,
+                                'content': content
+                                }}, True)
     return redirect(url_for('home'))
+
+
+@notes_app.route('/new')
+def add_note():
+    content = request.args.get('content')
+    note_title = request.args.get('title')    
+    notes.insert_one({'title' : note_title, 'content': content}, True)
+    return redirect(url_for('home'))
+
 
 @notes_app.route('/reset')
 def reset_notes():
-    session['note_dict'] = {}
-    session.modified = True
+    notes.delete_many({})
+    session.clear()
     flash('reset notes')
-    return redirect(url_for('logout'))
+    return redirect(url_for('new_note'))
+
 
 @notes_app.route('/debug')
 def debug_info():
     flash(str(session))
     return redirect(url_for('home'))
+
 
 @notes_app.route('/login')  
 def login():
@@ -77,11 +86,7 @@ def success():
         print(session['email'])
         session.modified = True
         flash('You were successfully logged in')
-        if 'note_dict' in session:
-            return redirect(url_for('home'))  
-        else:
-            return redirect(url_for('reset_notes'))
-        
+    return redirect(url_for('home'))
     
  
 @notes_app.route('/logout')  
--- static/dialogs.js
+++ static/dialogs.js
@@ -1,22 +1,110 @@
 const loginButton = document.getElementById('loginButton');
 const loginDialog = document.getElementById('loginDialog');
-const editButton = document.getElementById('editButton');
-const editDialog = document.getElementById('editDialog');
+const notes = document.querySelectorAll('.noteForm');
+const deletes = document.querySelectorAll('.delete');
+const flashes = document.getElementById('flashes');
+const cornerButton = document.getElementById('cornerButton')
+const nav = document.getElementById('nav')
+const closeDebug = document.getElementById('closeDebug')
+const hiddenForm = document.getElementById('hiddenForm')
+
+
+
+function delayFade(element, time, remove=false) {
+    let bro1 = (element.nextElementSibling)
+    let bro2 = (element.previousElementSibling)
+    bro1.setAttribute('transform' , "translate( 0px " + element.offsetHeight + "px)")
+    bro2.setAttribute('transform' , "translate( 0px -" + element.offsetHeight + "px)")
+    element.classList.toggle('fade');
+    if (remove) {
+        var timer = setTimeout(() => {
+
+            clearInterval(timer)
+            element.style.display = "none"
+            hiddenForm.src = element.id + "/delete"
+            wrapper.classList.toggle('squish')
+        }, time);
+    }
+}
+
+deletes.forEach(button => {
+    button.addEventListener('click', () => {
+        confirm('you sure about that?');
+        let note = document.getElementById(button.dataset.formid);
+        delayFade(note, 350, remove=true);
+    })
+})
 
 
 loginButton.addEventListener('click', () => {
     loginDialog.showModal();
 });
 
-emailConfirmBtn.addEventListener('click', (event) => {
-    favDialog.close();
+
+closeDebug.addEventListener('click', (event) => {
+    nav.close();
 });
 
-editButton.addEventListener('click', () => {
-    editButton.showModal();
-});
 
-editConfirmBtn.addEventListener('click', (event) => {
-    editDialog.close();
-});
 
+onLongPress(cornerButton, () => {
+    nav.showModal()
+  });
+
+
+
+function onLongPress(element, callback) {
+    let timer;
+    
+    element.addEventListener('touchstart', () => { 
+        timer = setTimeout(() => {
+        timer = null;
+        callback();
+        }, 500);
+    });
+    
+    function cancel() {
+        clearTimeout(timer);
+    }
+    
+    element.addEventListener('touchend', cancel);
+    element.addEventListener('touchmove', cancel);
+    }
+
+
+
+
+notes.forEach(note => {
+    note.addEventListener('change', () => {
+        
+        note.submit();
+        console.log('saving');
+
+
+      });
+    
+    let wrapper = document.getElementById('wrapper')
+    note.addEventListener('focusin', () => {
+     
+        document.getElementById('background').setAttribute('class', 'background blurry');
+        note.style.zIndex = "10"
+        note.style.opacity = "1"
+        note.style.height = "90vh"
+        note.scrollIntoView({behavior: 'smooth'})
+        note.querySelector('.contentInput').style.height = "85vh"
+        let backgroundColor = getComputedStyle(note).backgroundColor
+        note.style.webkitFilter = "drop-shadow(0px 0px 10px " + backgroundColor + ")"
+      });
+      
+    note.addEventListener('focusout', () => {
+        document.getElementById('background').setAttribute('class', 'background');
+        note.style.zIndex = "1"
+        note.style.opacity = ".69"
+        note.style.webkitFilter = ""
+        note.style.height = "initial"
+        
+        note.querySelector('.contentInput').style.height = "initial"
+      });
+
+
+    });
